6fbef6ca5891a7e564acfce2c96ad53b820083d2
tildozer <actninswitch@gmail.com>
2025-04-04 16:58:35 -0600

    added bank system to be able to buy turrets and enemy deaths now make money

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,8 +1,8 @@
 extends PathFollow3D
 
 @export_range(50, 200) var max_health := 50
-
 @export_range(1, 50, 0.5) var speed: float = 5.0
+@export var gold_value := 15
 
 var current_health : int:
 	set(health_in):
@@ -11,10 +11,12 @@ var current_health : int:
 		current_health = health_in
 		print("Health is now: ", current_health)
 		if current_health < 1:
+			bank.gold += gold_value
 			queue_free()
 
 @onready var animation_player = $AnimationPlayer 
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 
 func _ready() -> void:
 	current_health = max_health
